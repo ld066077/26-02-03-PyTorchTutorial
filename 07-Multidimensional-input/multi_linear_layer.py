71b38f99e521e15c3901de43a3e6c64da1921039
ld066077 <ld066077@gmail.com>
2026-04-30 11:35:20 +0800

    add script

---
+++ 07-Multidimensional-input/multi_linear_layer.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+xy = np.loadtxt('test.csv.gz', delimiter=',', dtype=np.float32)
+x_data = torch.from_numpy(xy[:,:-1])
+y_data = torch.from_numpy(xy[:, [-1]]) #,[-1]确保y_data计算为矩阵不是向量(,-1)；x_data和y_data必须都为矩阵
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear1 = torch.nn.Linear(8, 6)
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.sigmoid(self.linear1(x))
+        x = self.sigmoid(self.linear2(x))
+        x = self.sigmoid(self.linear3(x))
+        return x
+
+model = Model()
+
+criterion = torch.nn.BCELoss(size_average=True)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in range(100):
+    # Forward
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    # Backward
+    optimizer.zero_grad()
+    loss.backward()
+
+    # Update
+    optimizer.step()
+
